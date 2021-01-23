c5072d64c81fafa544babe45135bc873265689e0
ShubhamUjinwal <shubhamujinwal114@gmail.com>
2021-01-22 19:41:12 -0500

    fixed colon

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,8 +1,6 @@
-import web from 'web-vitals';
-
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    web.then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
