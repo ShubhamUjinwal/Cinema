663922e217003e2109fffc6667f8619bac175321
ShubhamUjinwal <shubhamujinwal114@gmail.com>
2021-01-23 20:54:04 -0500

    added terraform IAC and also to circleci pipeline

---
+++ deploy/s3.tf
@@ -0,0 +1,58 @@
+############################
+# S3 RESOURCES
+############################
+
+resource "aws_s3_bucket" "cinema_app_s3_bucket" {
+  bucket        = "${local.prefix}-app"
+  acl           = "public-read"
+  force_destroy = true
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "PublicReadGetObject",
+        "Action": [
+          "s3:GetObject"
+        ],
+        "Effect": "Allow",
+        "Resource": "arn:aws:s3:::${local.prefix}-app/*",
+        "Principal": "*"
+      } 
+    ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  tags = local.common_tags
+}
+
+resource "aws_s3_bucket_policy" "cinema_app_s3_bucket_policy" {
+  bucket = aws_s3_bucket.cinema_app_s3_bucket.id
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "PublicReadGetObject",
+        "Action": [
+          "s3:GetObject"
+        ],
+        "Effect": "Allow",
+        "Resource": "arn:aws:s3:::${local.prefix}-app/*",
+        "Principal": "*"
+      } 
+    ]
+}
+POLICY
+}
