cb1cc445314ecbc8bad091c2ce4a7cedb0801969
ShubhamUjinwal <shubhamujinwal114@gmail.com>
2021-12-19 18:39:01 -0500

    pull amplify project

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "cognito821fb1a0": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string"
+        }
+    },
+    "analytics": {
+        "cinema": {
+            "Region": "string",
+            "Id": "string",
+            "appName": "string"
+        }
+    }
+}
